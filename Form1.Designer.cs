233790a67c8a52b7b628b2a0c1e5674b7252b8c2
danilozenor <161782001+danilozenor@users.noreply.github.com>
2024-05-01 15:37:26 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace TpFinalNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTp1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnCalculer = new System.Windows.Forms.Button();
+            this.btnEffacer = new System.Windows.Forms.Button();
+            this.btnQuiter = new System.Windows.Forms.Button();
+            this.tbTp2 = new System.Windows.Forms.TextBox();
+            this.tbTp3 = new System.Windows.Forms.TextBox();
+            this.tbEprat = new System.Windows.Forms.TextBox();
+            this.tpEfin = new System.Windows.Forms.TextBox();
+            this.totaltp = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbNoteG = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbTp1
+            // 
+            this.tbTp1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTp1.Location = new System.Drawing.Point(216, 129);
+            this.tbTp1.Name = "tbTp1";
+            this.tbTp1.Size = new System.Drawing.Size(141, 35);
+            this.tbTp1.TabIndex = 0;
+            this.tbTp1.TextChanged += new System.EventHandler(this.tbTp1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(95, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TP1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(95, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TP2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(95, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "TP3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(491, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(241, 28);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "EXAMEN PRATIQUE";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(491, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(204, 28);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "EXAMEN FINALE";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(496, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(195, 28);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "NOTE GLOBALE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(209, 386);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(139, 28);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "TOTALE TP";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(645, 386);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(264, 28);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "LETTRE A ATTRIBUER";
+            // 
+            // btnCalculer
+            // 
+            this.btnCalculer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnCalculer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculer.ForeColor = System.Drawing.Color.Fuchsia;
+            this.btnCalculer.Location = new System.Drawing.Point(209, 533);
+            this.btnCalculer.Name = "btnCalculer";
+            this.btnCalculer.Size = new System.Drawing.Size(148, 53);
+            this.btnCalculer.TabIndex = 16;
+            this.btnCalculer.Text = "Calculer";
+            this.btnCalculer.UseVisualStyleBackColor = false;
+            this.btnCalculer.Click += new System.EventHandler(this.btnCalculer_Click);
+            // 
+            // btnEffacer
+            // 
+            this.btnEffacer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnEffacer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEffacer.ForeColor = System.Drawing.Color.Fuchsia;
+            this.btnEffacer.Location = new System.Drawing.Point(501, 533);
+            this.btnEffacer.Name = "btnEffacer";
+            this.btnEffacer.Size = new System.Drawing.Size(148, 53);
+            this.btnEffacer.TabIndex = 17;
+            this.btnEffacer.Text = "Effacer";
+            this.btnEffacer.UseVisualStyleBackColor = false;
+            this.btnEffacer.Click += new System.EventHandler(this.btnEffacer_Click);
+            // 
+            // btnQuiter
+            // 
+            this.btnQuiter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnQuiter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuiter.ForeColor = System.Drawing.Color.Fuchsia;
+            this.btnQuiter.Location = new System.Drawing.Point(759, 533);
+            this.btnQuiter.Name = "btnQuiter";
+            this.btnQuiter.Size = new System.Drawing.Size(148, 53);
+            this.btnQuiter.TabIndex = 18;
+            this.btnQuiter.Text = "Quiter";
+            this.btnQuiter.UseVisualStyleBackColor = false;
+            this.btnQuiter.Click += new System.EventHandler(this.btnQuiter_Click);
+            // 
+            // tbTp2
+            // 
+            this.tbTp2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTp2.Location = new System.Drawing.Point(216, 224);
+            this.tbTp2.Name = "tbTp2";
+            this.tbTp2.Size = new System.Drawing.Size(141, 35);
+            this.tbTp2.TabIndex = 19;
+            // 
+            // tbTp3
+            // 
+            this.tbTp3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTp3.Location = new System.Drawing.Point(216, 305);
+            this.tbTp3.Name = "tbTp3";
+            this.tbTp3.Size = new System.Drawing.Size(141, 35);
+            this.tbTp3.TabIndex = 20;
+            // 
+            // tbEprat
+            // 
+            this.tbEprat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEprat.Location = new System.Drawing.Point(766, 120);
+            this.tbEprat.Name = "tbEprat";
+            this.tbEprat.Size = new System.Drawing.Size(141, 35);
+            this.tbEprat.TabIndex = 22;
+            // 
+            // tpEfin
+            // 
+            this.tpEfin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tpEfin.Location = new System.Drawing.Point(766, 217);
+            this.tpEfin.Name = "tpEfin";
+            this.tpEfin.Size = new System.Drawing.Size(141, 35);
+            this.tpEfin.TabIndex = 23;
+            // 
+            // totaltp
+            // 
+            this.totaltp.BackColor = System.Drawing.Color.MistyRose;
+            this.totaltp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totaltp.ForeColor = System.Drawing.Color.Black;
+            this.totaltp.Location = new System.Drawing.Point(211, 449);
+            this.totaltp.Name = "totaltp";
+            this.totaltp.Size = new System.Drawing.Size(146, 37);
+            this.totaltp.TabIndex = 25;
+            this.totaltp.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MistyRose;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(766, 435);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 37);
+            this.button3.TabIndex = 26;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbNoteG
+            // 
+            this.tbNoteG.BackColor = System.Drawing.Color.MistyRose;
+            this.tbNoteG.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNoteG.ForeColor = System.Drawing.Color.Black;
+            this.tbNoteG.Location = new System.Drawing.Point(771, 298);
+            this.tbNoteG.Name = "tbNoteG";
+            this.tbNoteG.Size = new System.Drawing.Size(146, 37);
+            this.tbNoteG.TabIndex = 28;
+            this.tbNoteG.UseVisualStyleBackColor = false;
+            this.tbNoteG.Click += new System.EventHandler(this.tbNoteG_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(1014, 669);
+            this.Controls.Add(this.tbNoteG);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.totaltp);
+            this.Controls.Add(this.tpEfin);
+            this.Controls.Add(this.tbEprat);
+            this.Controls.Add(this.tbTp3);
+            this.Controls.Add(this.tbTp2);
+            this.Controls.Add(this.btnQuiter);
+            this.Controls.Add(this.btnEffacer);
+            this.Controls.Add(this.btnCalculer);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTp1);
+            this.ForeColor = System.Drawing.Color.Yellow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTp1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnCalculer;
+        private System.Windows.Forms.Button btnEffacer;
+        private System.Windows.Forms.Button btnQuiter;
+        private System.Windows.Forms.TextBox tbTp2;
+        private System.Windows.Forms.TextBox tbTp3;
+        private System.Windows.Forms.TextBox tbEprat;
+        private System.Windows.Forms.TextBox tpEfin;
+        private System.Windows.Forms.Button totaltp;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button tbNoteG;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TpFinalNote
+{
+    public partial class Form1 : Form
+    {
+        private const double MaxTp1 = 25;
+        private const double MaxTp2 = 35;
+        private const double MaxTp3 = 40;
+        private const double MaxExamenPratique = 100;
+        private const double MaxExamenFinal = 100;
+     
+
+
+
+
+        string noteGlo;
+        double noteGlobal;
+
+
+        double valtp1, valtp2, valtp3;
+        int virgul = 2;
+        double ttp;
+        double valpra, valfinal;
+
+
+        double notAtp1;
+        double notAtp2;
+        double notAtp3;
+        double notApra;
+        double notAfin;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbTp1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void tbTotp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNoglo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void note_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNoteglo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculer_Click(object sender, EventArgs e)
+        {
+            string examprt = tbEprat.Text;
+            string tpfinal = tpEfin.Text;
+            string tp1 = tbTp1.Text;
+            string tp2 = tbTp2.Text;
+            string tp3 = tbTp3.Text;
+
+            if (double.TryParse(tp1 = tbTp1.Text, out valtp1))
+            {
+
+                if (valtp1 >= 0 && valtp1 <= 25)
+                {
+                    notAtp1 = (double)((valtp1 / 25) * 20);
+                }
+                else
+                {
+                    tbTp1.Text = string.Empty;
+                    MessageBox.Show("entrez une valeur positif et inferieur a 25 pour la note du TP1");
+                }
+            }
+            else
+            {
+                tbTp1.Text = string.Empty;
+                MessageBox.Show("la valeur du TP1 n est pas valide");
+            }
+            if (double.TryParse(tp2= tbTp2.Text, out valtp2))
+            {
+
+                if (valtp2 >= 0 && valtp2 <= 35)
+                {
+                    notAtp2 = (double)((valtp2 / 35) * 20);
+                }
+                else
+                {
+                    tbTp2.Text = string.Empty;
+                    MessageBox.Show("entrez une valeur positif et inferieur a 35  pour la note du TP2");
+                }
+            }
+            else
+            {
+                tbTp2.Text = string.Empty;
+                MessageBox.Show("la valeur du TP2 n est pas valide");
+            }
+            if (double.TryParse(tp3 = tbTp3.Text, out valtp3))
+            {
+
+                if (valtp3 >= 0 && valtp3 <= 40)
+                {
+                    notAtp3 = (double)((valtp3 / 40) * 20);
+                }
+                else
+                {
+                    tbTp3.Text = string.Empty;
+                    MessageBox.Show("entrez une valeur positif et inferieur a 40 pour la note du TP3");
+                }
+            }
+            else
+            {
+                tbTp3.Text = string.Empty;
+                MessageBox.Show("la valeur du TP3 n est pas valide");
+            }
+            if (double.TryParse(examprt= tbEprat.Text, out valpra))
+            {
+
+                if (valpra >= 0 && valpra <= 100)
+                {
+                    notApra = (double)((valpra / 100) * 20);
+                }
+                else
+                {
+                    tbEprat.Text = string.Empty;
+                    MessageBox.Show("entrez une valeur positif et inferieur a 100 pour la note de l examen pratique");
+                }
+            }
+            else
+            {
+                tbEprat.Text = string.Empty;
+                MessageBox.Show("la valeur de l examen pratique n est pas valide");
+            }
+            if (double.TryParse(tpfinal= tpEfin.Text, out valfinal))
+            {
+
+                if (valfinal >= 0 && valfinal <= 100)
+                {
+                    notAfin = (double)((valfinal / 100) * 20);
+
+                }
+                else
+                {
+                    tpEfin.Text = string.Empty;
+                    MessageBox.Show("entrez une valeur positif et inferieur a 100 pour la note de l examen final");
+                }
+            }
+            else
+            {
+                tpEfin.Text = string.Empty;
+                MessageBox.Show("la valeur de l examen final n est pas valide");
+            }
+            ttp = (double)((notAtp1 + notAtp2 + notAtp3));
+
+
+            totaltp.Text = ttp.ToString("N" + virgul);
+
+            if (double.TryParse(noteGlo, out noteGlobal))
+            {
+                                          
+            }
+            double ttpp = (double)(notAfin * 0.4 + notApra * 0.2);
+            noteGlobal = (double)((ttp / 3 * 0.4 + ttpp));
+            tbNoteG.Text = noteGlobal.ToString("N" + virgul);
+
+            if (noteGlobal < 10)
+            {
+                button3.Text = "E";
+            }
+            else
+            {
+                if (noteGlobal >= 10 && noteGlobal < 14)
+                {
+                    button3.Text = "D";
+                }
+                if (noteGlobal < 16 && noteGlobal >= 14)
+                {
+                    button3.Text = "C"; ;
+                }
+                if (noteGlobal < 18 && noteGlobal >= 16)
+                {
+                    button3.Text = "B";
+                }
+                if (noteGlobal >= 18)
+                {
+                    button3.Text = "A";
+                }
+            }
+
+        }
+
+        private void btnEffacer_Click(object sender, EventArgs e)
+        {
+            tbTp1.Text = "";
+            tbTp2.Text = "";
+            tbTp3.Text = "";
+            tbEprat.Text = "";
+            tpEfin.Text = "";
+            tbNoteG.Text = "";
+            totaltp.Text = "";
+            button3.Text = "";
+        }
+
+        private void btnQuiter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult quiter;
+                quiter = MessageBox.Show("voulez vous vraiment quiter ?", "",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (quiter == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void tbLettre_TextChanged(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void tbNoteG_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
